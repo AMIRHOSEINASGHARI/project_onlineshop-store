e6341a9910d774ab7d4bb61d4dc78b327f953bee
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-21 08:28:24 +0330

    productsCategory file

--- components/layout/MobileNavbar.jsx
+++ components/layout/MobileNavbar.jsx
@@ -8,20 +8,20 @@ const MobileNavbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="lg:hidden border-t-2 fixed w-full bottom-0 z-10 bg-white py-2 px-6 overflow-auto">
+    <nav className="lg:hidden border-t-2 fixed w-full bottom-0 z-10 bg-white overflow-auto">
       <div className="flex items-center justify-between gap-3">
         {navLinks.map((item) => (
           <Link
             href={item.link}
             key={item.name}
             className={`${
-              pathname === item.link ? "text-black" : "text-gray-500"
-            } flex flex-col items-center`}
+              pathname === item.link ? "text-black" : "text-gray-400"
+            } flex flex-col items-center py-2 px-6`}
           >
             <div className="text-[25px]">
               {pathname === item.link ? item.isActive : item.icon}
             </div>
-            <p className="text-[10px]">{item.name}</p>
+            <p className="text-[10px] font-semibold">{item.name}</p>
           </Link>
         ))}
       </div>
--- constants/index.js
+++ constants/index.js
@@ -7,6 +7,16 @@ import {
   BiSolidCart,
 } from "react-icons/bi";
 import { BsPerson, BsPersonFill } from "react-icons/bs";
+import { TbDeviceLaptop, TbDeviceGamepad2 } from "react-icons/tb";
+import { HiOutlineDeviceMobile } from "react-icons/hi";
+import { PiTelevisionSimpleBold } from "react-icons/pi";
+import { FiHeadphones, FiPrinter } from "react-icons/fi";
+import { CgAppleWatch } from "react-icons/cg";
+import { MdOutlinePhotoCamera } from "react-icons/md";
+import { FaComputer } from "react-icons/fa6";
+import { LuHardDrive } from "react-icons/lu";
+import { SlScreenTablet } from "react-icons/sl";
+import { LuSpeaker } from "react-icons/lu";
 
 export const navLinks = [
   {
@@ -32,5 +42,80 @@ export const navLinks = [
     isActive: <BsPersonFill />,
     name: "Profile",
     link: "/profile",
+  },
+];
+
+export const productsCategory = [
+  {
+    categoryName: "laptop",
+    icon: <TbDeviceLaptop />,
+    bgColor: "#f1f5f9",
+    textColor: "#64748b",
+  },
+  {
+    categoryName: "mobile",
+    icon: <HiOutlineDeviceMobile />,
+    bgColor: "#f4f4f5",
+    textColor: "#71717a",
+  },
+  {
+    categoryName: "television",
+    icon: <PiTelevisionSimpleBold />,
+    bgColor: "#fee2e2",
+    textColor: "#ef4444",
+  },
+  {
+    categoryName: "headphone",
+    icon: <FiHeadphones />,
+    bgColor: "#ffedd5",
+    textColor: "#f97316",
+  },
+  {
+    categoryName: "printer",
+    icon: <FiPrinter />,
+    bgColor: "#fef9c3",
+    textColor: "#eab308",
+  },
+  {
+    categoryName: "watch",
+    icon: <CgAppleWatch />,
+    bgColor: "#ecfccb",
+    textColor: "#84cc16",
+  },
+  {
+    categoryName: "camera",
+    icon: <MdOutlinePhotoCamera />,
+    bgColor: "#ccfbf1",
+    textColor: "#14b8a6",
+  },
+  {
+    categoryName: "gaming",
+    icon: <TbDeviceGamepad2 />,
+    bgColor: "#dcfce7",
+    textColor: "#22c55e",
+  },
+  {
+    categoryName: "computer",
+    icon: <FaComputer />,
+    bgColor: "#cffafe",
+    textColor: "#06b6d4",
+  },
+  {
+    categoryName: "hard drive",
+    icon: <LuHardDrive />,
+    bgColor: "#e0e7ff",
+    textColor: "#6366f1",
+  },
+  {
+    categoryName: "tablet",
+    icon: <SlScreenTablet />,
+    bgColor: "#fae8ff",
+    textColor: "#d946ef",
+  },
+  {
+    categoryName: "speaker",
+    icon: <LuSpeaker />,
+    bgColor: "#ffe4e6",
+    textColor: "#f43f5e",
   },
 ];
