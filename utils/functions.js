6cb34c7c9c8083151c6d0bb5be5c3e9197e97fe3
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-22 12:01:57 +0330

    hash password function / user model created

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,5 @@
+import { compare, hash } from "bcryptjs";
+
 export const createSlug = (title) => {
   const slug = title.split(" ").join("-");
   return slug;
@@ -7,4 +9,14 @@ export const reducePrice = (discount, price) => {
   const discountValue = (price * discount) / 100;
   const finalValue = price - discountValue;
   return finalValue;
+};
+
+export const hashPassword = async (password) => {
+  const hashPassword = await hash(password, 12);
+  return hashPassword;
+};
+
+export const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
 };
