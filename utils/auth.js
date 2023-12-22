09a9062b48eb4f909ae38efa6b4f56152f88e00b
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-22 12:30:32 +0330

    [...nextAuth] file created

---
+++ utils/auth.js
@@ -0,0 +1,55 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { StoreShopUser } from "./models/user";
+import connectDB from "./connectDB";
+
+export const authOptions = {
+  session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        await connectDB();
+        const { username, password } = credentials;
+        const isUser = await StoreShopUser.findOne({ username });
+
+        if (!isUser) {
+          throw new Error("User not found");
+        } else if (isUser.password !== password) {
+          throw new Error("Username or password is incorrect!");
+        } else {
+          const user = {
+            email: username,
+            name: isUser.displayName,
+          };
+          return user;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token }) {
+      await connectDB();
+      const isUser = await StoreShopUser.findOne({
+        username: token.email,
+      });
+      if (isUser) {
+        token.username = isUser.username;
+        delete token.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      try {
+        const newSession = {
+          ...session,
+          user: {
+            name: token.name,
+            username: token.username,
+          },
+        };
+        return newSession;
+      } catch (error) {
+        return session;
+      }
+    },
+  },
+};
