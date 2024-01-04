e43a4002c9196f5c6ccfb7d596738192b257012d
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-01-04 16:35:18 +0330

    profile page DesktopLayout

--- components/layout/DesktopLayout.jsx
+++ components/layout/DesktopLayout.jsx
@@ -1,10 +1,31 @@
+import { profileLinks } from "@/constants";
+import Link from "next/link";
 import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+import LogoutButton from "../pages/profile/LogoutButton";
 
 const DesktopLayout = ({ children }) => {
   return (
-    <div className="max-lg:hidden">
-      <main>{children}</main>
-      <aside></aside>
+    <div className="max-lg:hidden flex gap-5">
+      <aside className="w-[300px] border rounded-xl p-3">
+        {profileLinks.map((el, i) => (
+          <Link
+            href={el.link}
+            key={el.name}
+            className="flex items-center justify-between py-3 border-b border-gray-100 hover:bg-gray-100 rounded-lg px-3"
+          >
+            <div className="flex items-center gap-5">
+              <div className="text-[25px]">{el.icon}</div>
+              <p className="font-medium text-[14px]">{el.name}</p>
+            </div>
+            <IoIosArrowForward className="text-gray-500" />
+          </Link>
+        ))}
+        <div>
+          <LogoutButton />
+        </div>
+      </aside>
+      <main className="w-full">{children}</main>
     </div>
   );
 };
--- components/pages/profile/LogoutButton.jsx
+++ components/pages/profile/LogoutButton.jsx
@@ -8,14 +8,14 @@ import { IoIosArrowForward } from "react-icons/io";
 const LogoutButton = () => {
   return (
     <button
-      className="flex items-center justify-between py-3 w-full"
+      className="flex items-center justify-between py-3 lg:px-3 w-full hover:bg-gray-100 lg:rounded-lg"
       onClick={() => signOut()}
     >
       <div className="flex items-center gap-5">
-        <div className="text-[20px]">
+        <div className="text-[20px] lg:text-[25px]">
           <BiLogOut />
         </div>
-        <p className="font-medium text-[12px]">Logout</p>
+        <p className="font-medium text-[12px] lg:text-[14px]">Logout</p>
       </div>
       <IoIosArrowForward className="text-gray-500" />
     </button>
