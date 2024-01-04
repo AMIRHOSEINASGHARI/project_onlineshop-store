1ebfd71119c63cc0a9893bf3ba9f7f1bfd808cff
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-01-04 16:25:31 +0330

    C:/Program Files/Git/profile route

--- app/(pages)/profile/page.jsx
+++ app/(pages)/profile/page.jsx
@@ -1,7 +1,27 @@
 import ProfilePage from "@/components/pages/profile/ProfilePage";
+import { authOptions } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import { StoreShopUser } from "@/utils/models/user";
+import { getServerSession } from "next-auth";
 
-const Profile = () => {
-  return <ProfilePage />;
+const Profile = async () => {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  const user = await StoreShopUser.findOne({
+    username: session.user.username,
+  }).select([
+    "_id",
+    "username",
+    "displayName",
+    "phoneNumber",
+    "address",
+    "orders",
+    "likes",
+    "comments",
+    "createdAt",
+  ]);
+
+  return <ProfilePage {...user} />;
 };
 
 export default Profile;
--- components/pages/profile/ProfilePage.jsx
+++ components/pages/profile/ProfilePage.jsx
@@ -1,9 +1,39 @@
 import React from "react";
-import MobileView from "./MobileView";
-import DesktopView from "./DesktopView";
+import Link from "next/link";
+import { CiEdit } from "react-icons/ci";
+import { IoIosArrowForward } from "react-icons/io";
 
-const ProfilePage = () => {
-  return <div>profile page</div>;
+const ProfilePage = (props) => {
+  const { displayName, phoneNumber, orders, createdAt } = props._doc;
+  return (
+    <div>
+      <div>
+        <div className="flex justify-between">
+          <h1 className="text-[18px] font-medium capitalize">{displayName}</h1>{" "}
+          <Link href="/profile/edit">
+            <CiEdit className="text-[25px] text-blue-400" />
+          </Link>
+        </div>
+        <p className="text-[12px] text-gray-400 mb-2">{phoneNumber}</p>
+        <p className="text-[12px]">
+          Joined At: {createdAt.toLocaleDateString()}
+        </p>
+      </div>
+      <hr className="my-3" />
+      <div className="cardShadow p-3 rounded-xl">
+        <div className="flex items-center justify-between mb-5">
+          <h1 className="font-bold text-[20px]">My Orders</h1>
+          <Link
+            href="/profile/orders"
+            className="flex items-center gap-1 text-blue-400 text-[12px]"
+          >
+            See All <IoIosArrowForward />
+          </Link>
+        </div>
+        <span>{orders.length}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
