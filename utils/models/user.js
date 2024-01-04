59a8a2534be8f6ffe930b9843bc4ada7c9be5cd0
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-01-04 14:29:43 +0330

    changing user schema

--- utils/models/user.js
+++ utils/models/user.js
@@ -1,9 +1,16 @@
 const { Schema, models, model } = require("mongoose");
 
 const userSchema = new Schema({
-  username: String,
-  displayName: String,
-  password: String,
+  username: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: Number },
+  address: { type: String },
+  orders: [{ type: Schema.Types.ObjectId, ref: "StoreShopOrder", default: [] }],
+  likes: [{ type: Schema.Types.ObjectId, ref: "StoreShopLike", default: [] }],
+  comments: [
+    { type: Schema.Types.ObjectId, ref: "StoreShopComment", default: [] },
+  ],
   createdAt: {
     type: Date,
     default: () => Date.now(),
