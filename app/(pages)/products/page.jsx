089934608977d9107d37cbe8239acb852c92f82c
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-02-13 02:52:43 +0330

    handling category filter in fetching data

--- app/(pages)/products/page.jsx
+++ app/(pages)/products/page.jsx
@@ -1,15 +1,25 @@
 import ProductsPage from "@/components/pages/products/ProductsPage";
 
-async function getData() {
+async function getData(searchParams) {
   const res = await fetch(`${process.env.API_URL}/api/store/products/all`, {
     next: { revalidate: 60 * 60 },
   });
   const data = await res.json();
-  return data;
+  if (!searchParams.category) {
+    return data;
+  } else {
+    const filteredData = data.data.filter(
+      (product) => product.category.toLowerCase() === searchParams.category
+    );
+    return {
+      ...data,
+      data: filteredData,
+    };
+  }
 }
 
-const Products = async () => {
-  const data = await getData();
+const Products = async ({ searchParams }) => {
+  const data = await getData(searchParams);
 
   if (!data.success) {
     return (
--- components/pages/products/ProductsPage.jsx
+++ components/pages/products/ProductsPage.jsx
@@ -7,11 +7,16 @@ const ProductsPage = ({ products }) => {
   return (
     <section className="pagePT mobilePx mobilePb mw lg:pt-[80px]">
       <Category />
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {products.map((product) => (
-          <ProductCard key={product?._id} {...product} />
-        ))}
-      </div>
+      {products.length === 0 && (
+        <h1 className="text-center mt-20">No Products Found!</h1>
+      )}
+      {products.length !== 0 && (
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {products.map((product) => (
+            <ProductCard key={product?._id} {...product} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
